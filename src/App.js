388801331fad31509d8dcd230ea9fad9778d9663
DelfinaMolter <delfi.molter@gmail.com>
2023-11-29 18:30:20 -0300

    application structure & component cleaning

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './img/logo.png';
+import './App.css';
+
+function App() {
+  return (
+    <div >
+      <header>
+        <img src={logo}  alt="logo" />
+      </header>
+    </div>
+  );
+}
+
+export default App;
