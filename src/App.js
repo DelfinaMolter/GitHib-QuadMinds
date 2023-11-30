6557d5353f5550101e2f1d23f3e77e26f9e64e9c
DelfinaMolter <delfi.molter@gmail.com>
2023-11-30 11:31:39 -0300

    config tailwind css

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import logo from './img/logo.png';
 import './App.css';
 
 function App() {
+  const number = '11'
+  console.log(number.padStart(2, '0'))    
   return (
-    <div >
+    <div className="flex flex-col items-center justify-center min-h-screen" >
       <header>
         <img src={logo}  alt="logo" />
+        <h1 class="text-3xl font-bold text-white">
+          Hello world!
+        </h1>
       </header>
     </div>
   );
